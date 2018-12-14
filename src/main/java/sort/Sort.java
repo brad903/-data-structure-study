98d43f054475682d4548dae1e5c9df9fe8492ad4
Brad Lee <brad903@naver.com>
2018-12-14 16:33:38 +0900

    bubble sort 구현 및 시간 계산

--- src/main/java/sort/Sort.java
+++ src/main/java/sort/Sort.java
@@ -5,13 +5,27 @@ import java.util.stream.IntStream;
 
 public class Sort {
 
-    // insert sort
-    public void sort(int[] arr) {
+    // insert insertionSort
+    public void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public void bubbleSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            int temp = arr[i];
-            for (int j = i - 1; j >= 0; j--) {
-                if (arr[j] > temp) swap(arr, j, j + 1);
-                else break;
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j - 1] > arr[j]) {
+                    swap(arr, j, j - 1);
+                }
             }
         }
     }
--- src/test/java/sort/SortTest.java
+++ src/test/java/sort/SortTest.java
@@ -2,14 +2,11 @@ package sort;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.slf4j.Logger;
 
-import static org.junit.Assert.*;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.slf4j.LoggerFactory.getLogger;
+import java.util.Arrays;
+
 
 public class SortTest {
-    private static final Logger log = getLogger(SortTest.class);
 
     Sort sort;
     final int size = 10;
@@ -19,33 +16,112 @@ public class SortTest {
         sort = new Sort();
     }
 
-    @Test
-    public void genArray() {
-        int[] array = sort.genArray(5);
-        assertThat(array[4]).isEqualTo(4);
-    }
+//    @Test
+//    public void genArray() {
+//        int[] array = sort.genArray(5);
+//        assertThat(array[4]).isEqualTo(4);
+//    }
+//
+//    @Test(expected = ArrayIndexOutOfBoundsException.class)
+//    public void genArray_범위넘을때() {
+//        int[] array = sort.genArray(5);
+//        assertThat(array[5]).isEqualTo(5);
+//    }
+//
+//    @Test
+//    public void shuffle() {
+//        int[] array = sort.genArray(5);
+//        int origin = array[0];
+//        sort.shuffle(array);
+//        int after = array[0];
+//        assertThat(origin == after).isEqualTo(false);
+//    }
+//
+//    @Test
+//    public void insertSort() {
+//        int[] array = sort.genArray(20);
+//        sort.shuffle(array);
+//        sort.printArray(array);
+//        sort.insertionSort(array);
+//        sort.printArray(array);
+//    }
+//
+//    @Test
+//    public void bubbleSort() {
+//        int[] array = sort.genArray(20);
+//        sort.shuffle(array);
+//        sort.printArray(array);
+//        sort.bubbleSort(array);
+//        sort.printArray(array);
+//    }
 
-    @Test(expected = ArrayIndexOutOfBoundsException.class)
-    public void genArray_범위넘을때() {
-        int[] array = sort.genArray(5);
-        assertThat(array[5]).isEqualTo(5);
-    }
-
     @Test
-    public void shuffle() {
-        int[] array = sort.genArray(5);
-        int origin = array[0];
+    public void buble_insertion() {
+        int[] array = sort.genArray(50000);
         sort.shuffle(array);
-        int after = array[0];
-        assertThat(origin == after).isEqualTo(false);
+        long start = System.currentTimeMillis();
+        sort.bubbleSort(array);
+        long middle = System.currentTimeMillis();
+        System.out.println("bubbleFirst : " + (middle - start));
+        sort.insertionSort(array);
+        long end = System.currentTimeMillis();
+        System.out.println("insertionLast : " + (end - middle));
+        System.out.println("-----------------------------------");
     }
 
     @Test
-    public void sort() {
-        int[] array = sort.genArray(20);
+    public void insertion_bubble() {
+        int[] array = sort.genArray(50000);
         sort.shuffle(array);
-        sort.printArray(array);
-        sort.sort(array);
-        sort.printArray(array);
+        long start = System.currentTimeMillis();
+        sort.insertionSort(array);
+        long middle = System.currentTimeMillis();
+        System.out.println("insertionFirst : " + (middle - start));
+        sort.bubbleSort(array);
+        long end = System.currentTimeMillis();
+        System.out.println("bubbleLast : " + (end - middle));
+        System.out.println("-----------------------------------");
+    }
+
+    @Test
+    public void insertion_insertion() {
+        int[] array = sort.genArray(50000);
+        sort.shuffle(array);
+        long start = System.currentTimeMillis();
+        sort.insertionSort(array);
+        long middle = System.currentTimeMillis();
+        System.out.println("insertionFirst : " + (middle - start));
+        sort.insertionSort(array);
+        long end = System.currentTimeMillis();
+        System.out.println("insertionTwice : " + (end - middle));
+        System.out.println("-----------------------------------");
+    }
+
+    @Test
+    public void bubble_bubble() {
+        int[] array = sort.genArray(50000);
+        sort.shuffle(array);
+        long start = System.currentTimeMillis();
+        sort.bubbleSort(array);
+        long middle = System.currentTimeMillis();
+        System.out.println("bubbleFirst : " + (middle - start));
+        sort.bubbleSort(array);
+        long end = System.currentTimeMillis();
+        System.out.println("bubbleTwice : " + (end - middle));
+        System.out.println("-----------------------------------");
+    }
+
+    @Test
+    public void array_sort() {
+        int[] array = sort.genArray(50000);
+        sort.shuffle(array);
+        long start = System.currentTimeMillis();
+        Arrays.sort(array);
+        long middle = System.currentTimeMillis();
+        System.out.println("arraySortFirst : " + (middle - start));
+        Arrays.sort(array);
+        long end = System.currentTimeMillis();
+        System.out.println("arraySortTwice : " + (end - middle));
+        System.out.println("-----------------------------------");
     }
 }
